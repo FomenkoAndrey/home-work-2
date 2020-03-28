e4c24c08a24416cd94cbebe41e91bce14b942ecc
Alecto <fomenkoandrey1978@gmail.com>
2020-03-28 10:52:04 +0200

    временное сохранение. глобальное обновление структуры.

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+'use strict';
+
+require('../../gulp/gulp-init.js')({HTML: '.'});
+
+const comb = require('../../gulp/tasks/comb.js'),
+    scssDC     = require('../../gulp/tasks/scss.js').scssDC,
+    mincss   = require('../../gulp/tasks/mincss.js'),
+    uglifyes = require('../../gulp/tasks/uglify.js').uglifyes,
+    { sync, syncInit } = require('../../gulp/tasks/sync.js');
+
+function watchFiles () {
+    syncInit();
+    watch($.PATH.scss.files, series(scssDC));
+    watch([$.PATH.js.files, '!' + $.PATH.js.filesMin], series(uglifyes, sync));
+    watch($.PATH.html.files, sync);
+    watch($.PATH.css.files, sync);
+}
+
+task('combScss', comb);
+task('uglifyEs6', series(uglifyes, sync));
+task('sass', series(scssDC));
+task('watch', watchFiles);
